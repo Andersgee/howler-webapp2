0cb473b775e1903a234abd6e0af703706fe913a4
andy <andersgee@gmail.com>
2024-01-16 14:59:05 +0100

    so turns out can not set style in js if using map_id, and can no use advancedmarker (and prob other things) if using map_id, have to associate the map_id with a style on google project

--- src/components/GoogleMaps/control-infowindow.tsx
+++ src/components/GoogleMaps/control-infowindow.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { api } from "#src/hooks/api";
+import { IconWhat, IconWhen, IconWhere, IconWho } from "#src/icons";
 import { IconLoadingSpinner } from "#src/icons/special";
 import { useStore } from "#src/store";
 import { hashidFromId } from "#src/utils/hashid";
-import { JSONE } from "#src/utils/jsone";
 import Link from "next/link";
 import { createPortal } from "react-dom";
+import { PrettyDate } from "../PrettyDate";
 
 export function ControlInfoWindow() {
   const googleMaps = useStore.use.googleMaps();
@@ -38,8 +39,30 @@ function Content() {
       className="block bg-color-unthemed-neutral-0 text-color-unthemed-neutral-800 hover:bg-color-unthemed-neutral-300"
       href={`/event/${hashidFromId(event.id)}`}
     >
-      <div className="mb-2 text-lg font-medium text-color-unthemed-neutral-1000">{event.title}</div>
-      <div className="w-48">{JSONE.stringify(event, 2)}</div>
+      <div className="space-y-6">
+        <div className="flex items-center gap-2">
+          <IconWhat />
+          <div className="w-11 shrink-0">What</div>
+          <h1 className="m-0 capitalize-first">{event.title}</h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <IconWhere />
+          <div className="w-11 shrink-0">Where</div>
+          <div className="capitalize-first">{event.locationName ?? "anywhere"}</div>
+        </div>
+        <div className="flex items-center gap-2">
+          <IconWhen />
+          <div className="w-11 shrink-0">When</div>
+          <div>
+            <PrettyDate date={event.date} />
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <IconWho />
+          <div className="w-11 shrink-0">Who</div>
+          <div>anyone</div>
+        </div>
+      </div>
     </Link>
   );
 }
--- src/components/GoogleMaps/google-maps-class.ts
+++ src/components/GoogleMaps/google-maps-class.ts
@@ -104,6 +104,9 @@ export class GoogleMapsClass {
         //  //mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain"],
         //  mapTypeIds: ["roadmap", "satellite"],
         //},
+        //styling
+        //https://developers.google.com/maps/documentation/javascript/style-reference#stylers
+        //styles: STYLES_DARK,
       });
 
       this.primaryPin = new PinElement({
@@ -200,3 +203,85 @@ export class GoogleMapsClass {
 
 //any way to
 type EventClick = { latLng: google.maps.LatLng };
+
+////https://developers.google.com/maps/documentation/javascript/examples/style-array#maps_style_array-typescript
+const STYLES_DARK: google.maps.MapTypeStyle[] = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }],
+  },
+];
