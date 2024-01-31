987d74c6dd25d2ed813e3c1b1fff5e0beb1d62b2
andy <andersgee@gmail.com>
2024-01-31 02:37:42 +0100

    swap main image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = seo({
   title: "Howler",
   description: "Quickly find/plan stuff to do with friends, or with anyone really.",
   url: "/",
-  image: "/andyfx.png",
+  image: "/Howler.png",
 });
 
 export const runtime = "edge";
