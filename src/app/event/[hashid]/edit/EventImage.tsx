7c063ae9a71d25ebbbadd832c1657330786b85db
andy <andersgee@gmail.com>
2024-02-01 03:25:15 +0100

    input capture attribute

--- src/app/event/[hashid]/edit/EventImage.tsx
+++ src/app/event/[hashid]/edit/EventImage.tsx
@@ -48,6 +48,7 @@ export function EventImage({ event, className }: Props) {
         <input
           type="file"
           accept="image/png, image/jpeg"
+          capture="environment"
           className="sr-only"
           disabled={isUploading}
           onChange={(e) => {
