2a14302a1da14a5ab0415078d4c727f59d98b8ef
andy <andersgee@gmail.com>
2024-02-04 20:30:36 +0100

    fix list style on terms

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -3,18 +3,12 @@ export default function Page() {
     <div className="flex justify-center">
       <div className="px-2">
         <h1 className="mb-6 text-2xl">terms of service</h1>
-        <ul className="list-inside list-disc">
+        <ul className="list-inside list-disc space-y-2">
+          <li>Howler is a web app that lets people create/organize/find events.</li>
+          <li>Dont spam/abuse the service or create offensive content.</li>
           <li>
-            <p>Howler is a web app that lets people create/organize/find events.</p>
-          </li>
-          <li>
-            <p>Dont spam/abuse the service or create offensive content.</p>
-          </li>
-          <li>
-            <p>
-              Misuse of the service in any way deemed inappropriate by the creators/developers may result in removal of
-              your account.
-            </p>
+            Misuse of the service in any way deemed inappropriate by the creators/developers may result in removal of
+            your account.
           </li>
         </ul>
       </div>
