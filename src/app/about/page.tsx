d8f207ffb947483d3d19373c2e8fe9304328eabb
andy <andersgee@gmail.com>
2024-02-07 02:20:18 +0100

    perhaps excluding some stuff from serach and files makes vscode faster?..

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center">
+      <div className="px-2">
+        <h1 className="mb-6 text-2xl">About</h1>
+        <p>Effective Date: 2024-02-06</p>
+      </div>
+    </div>
+  );
+}
