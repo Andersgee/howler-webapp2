5dfd5ca9a85d800509fb623e0d7f06388da982ec
andy <andersgee@gmail.com>
2024-05-03 01:24:07 +0200

    ok, that works. in fact its the same payload so it passes zod validation and turns up as a foreground notification, which ofc does not do router.push or stuff like that

--- scripts/debugnotify.ts
+++ scripts/debugnotify.ts
@@ -13,7 +13,7 @@ async function main() {
 
   const userIds = [user.id];
   await notify(userIds, {
-    title: `welp1`,
+    title: `kek2`,
     body: "Test body",
     relativeLink: "/event/R3AxD",
     //icon: user.image ?? undefined,
