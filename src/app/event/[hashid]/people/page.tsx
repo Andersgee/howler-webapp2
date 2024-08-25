197964bf659125fdcb2448cb82b2778ecc098ea3
andy <andersgee@gmail.com>
2024-08-25 11:50:38 +0200

    make /people dynamic, apiRscPublic is using cache: no-store which disables ppr

--- src/app/event/[hashid]/people/page.tsx
+++ src/app/event/[hashid]/people/page.tsx
@@ -1,9 +1,17 @@
 import { Shell } from "#src/components/Shell";
 import { UserImage32x32 } from "#src/components/user/UserImage";
 import { apiRscPublic } from "#src/trpc/api-rsc-public";
+import { buttonVariants } from "#src/ui/button";
 import { hashidFromId, idFromHashid } from "#src/utils/hashid";
+import { type Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export function generateMetadata(): Metadata {
+  return { title: "People" };
+}
 
 type Props = {
   searchParams: Record<string, string | string[] | undefined>;
@@ -33,6 +41,9 @@ export default async function Page({ params }: Props) {
           </Link>
         ))}
       </div>
+      <Link href={`/event/${params.hashid}`} className={buttonVariants({ variant: "icon" })}>
+        Back
+      </Link>
     </Shell>
   );
 }
