72d92b70d47b18c898c506d0d2b9a1349d0ffbd6
andy <andersgee@gmail.com>
2024-08-28 22:05:15 +0200

    removed forwardRef from ui/switch

--- src/ui/switch.tsx
+++ src/ui/switch.tsx
@@ -1,28 +1,26 @@
 "use client";
 
-import * as React from "react";
+import { type ComponentPropsWithRef } from "react";
 import * as SwitchPrimitives from "@radix-ui/react-switch";
 import { cn } from "#src/utils/cn";
 
-const Switch = React.forwardRef<
-  React.ElementRef<typeof SwitchPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
->(({ className, ...props }, ref) => (
-  <SwitchPrimitives.Root
-    className={cn(
-      "peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-color-neutral-200 outline-none transition-colors focus-visible:focusring disabled:cursor-not-allowed disabled:opacity-50 data-state-checked:bg-color-primary-500 data-state-unchecked:bg-color-primary-100",
-      className
-    )}
-    {...props}
-    ref={ref}
-  >
-    <SwitchPrimitives.Thumb
+function Switch({ className, ref, ...props }: ComponentPropsWithRef<typeof SwitchPrimitives.Root>) {
+  return (
+    <SwitchPrimitives.Root
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-color-primary-500 shadow-lg ring-0 transition-transform data-state-checked:translate-x-5 data-state-checked:bg-color-primary-800 data-state-unchecked:translate-x-0"
+        "peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-color-neutral-200 outline-none transition-colors focus-visible:focusring disabled:cursor-not-allowed disabled:opacity-50 data-state-checked:bg-color-primary-500 data-state-unchecked:bg-color-primary-100",
+        className
       )}
-    />
-  </SwitchPrimitives.Root>
-));
-Switch.displayName = SwitchPrimitives.Root.displayName;
+      {...props}
+      ref={ref}
+    >
+      <SwitchPrimitives.Thumb
+        className={cn(
+          "pointer-events-none block h-5 w-5 rounded-full bg-color-primary-500 shadow-lg ring-0 transition-transform data-state-checked:translate-x-5 data-state-checked:bg-color-primary-800 data-state-unchecked:translate-x-0"
+        )}
+      />
+    </SwitchPrimitives.Root>
+  );
+}
 
 export { Switch };
