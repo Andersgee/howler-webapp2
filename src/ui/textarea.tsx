a4a0bc07f3607bf0bf68b0e7bbbea3e176b8c33a
andy <andersgee@gmail.com>
2024-08-28 22:08:30 +0200

    removed forwardRef from ui/textarea

--- src/ui/textarea.tsx
+++ src/ui/textarea.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { type RefObject } from "react";
 import { cn } from "#src/utils/cn";
 
 export type Props = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
@@ -6,9 +6,6 @@ export type Props = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 export const inputElementStyles =
   "w-full border-color-neutral-400 bg-color-neutral-0 text-color-neutral-1000 placeholder:text-color-neutral-500 disabled:bg-color-neutral-100 disabled:text-color-neutral-400 rounded-md border px-3 py-3 text-sm disabled:cursor-not-allowed outline-none";
 
-const TextArea = React.forwardRef<HTMLTextAreaElement, Props>(({ className, ...props }, ref) => {
+export function TextArea({ className, ref, ...props }: Props & { ref?: RefObject<HTMLTextAreaElement> }) {
   return <textarea className={cn(inputElementStyles, "focus-visible:focusring", className)} ref={ref} {...props} />;
-});
-TextArea.displayName = "TextArea";
-
-export { TextArea };
+}
