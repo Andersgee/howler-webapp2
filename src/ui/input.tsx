4807e843c5a2ae7dea4772a1c0e12ffb30f5e302
andy <andersgee@gmail.com>
2024-08-28 22:46:53 +0200

    removed forwardRef from ui/input

--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { type RefObject } from "react";
 import { cn } from "#src/utils/cn";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
@@ -6,11 +6,8 @@ export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 export const inputElementStyles =
   "w-56 border-color-neutral-400 bg-color-neutral-0 text-color-neutral-1000 placeholder:text-color-neutral-500 disabled:bg-color-neutral-100 disabled:text-color-neutral-400 flex h-10 rounded-md border px-3 py-6 text-sm  disabled:cursor-not-allowed outline-none";
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+export function Input({ className, type, ref, ...props }: InputProps & { ref?: RefObject<HTMLInputElement> }) {
   return (
     <input type={type} className={cn(inputElementStyles, "focus-visible:focusring", className)} ref={ref} {...props} />
   );
-});
-Input.displayName = "Input";
-
-export { Input };
+}
