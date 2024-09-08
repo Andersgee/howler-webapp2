97911ed828f079064a4b12eb8873e572ce935dba
andy <andersgee@gmail.com>
2024-09-08 14:54:51 +0200

    comment out useServiceWorker and useCloudMessaging from root layout

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import PlausibleProvider from "next-plausible";
-import { useCloudMessaging } from "#src/hooks/useCloudMessaging";
 import { useGetSession } from "#src/hooks/useGetSession";
-import { useServiceWorker } from "#src/hooks/userServiceWorker";
 import { TrpcProvider } from "./Trpc";
+//import { useCloudMessaging } from "#src/hooks/useCloudMessaging";
+//import { useServiceWorker } from "#src/hooks/userServiceWorker";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   useGetSession();
-  const registration = useServiceWorker();
-  useCloudMessaging(registration);
+  //const registration = useServiceWorker();
+  //useCloudMessaging(registration);
   return (
     <PlausibleProvider domain="howler.andyfx.net">
       <TrpcProvider>{children}</TrpcProvider>
