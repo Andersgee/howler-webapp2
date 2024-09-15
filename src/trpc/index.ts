c673e15d6bc02bb9e79fdef47b248efddefe3666
andy <andersgee@gmail.com>
2024-09-15 18:57:03 +0200

    re

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,7 +3,6 @@ import { postRouter } from "./routers/post";
 import { userRouter } from "./routers/user";
 import { eventRouter } from "./routers/event";
 import { geocodeRouter } from "./routers/geocode";
-import { fcmRouter } from "./routers/fcm";
 import { notificationRouter } from "./routers/notification";
 import { commentRouter } from "./routers/comment";
 import { replyRouter } from "./routers/reply";
@@ -13,7 +12,6 @@ export const trpcRouter = createTRPCRouter({
   post: postRouter,
   event: eventRouter,
   geocode: geocodeRouter,
-  fcm: fcmRouter,
   notification: notificationRouter,
   comment: commentRouter,
   reply: replyRouter,
