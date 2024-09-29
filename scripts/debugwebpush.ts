45be01eca9c968f6d3fb7e7b6448a4f2fcc1ffa5
andy <andersgee@gmail.com>
2024-09-29 12:48:55 +0200

    test script, yep recieving notification always, even if app is open

--- scripts/debugwebpush.ts
+++ scripts/debugwebpush.ts
@@ -1,17 +1,25 @@
 import "dotenv/config";
 import "#src/utils/validate-process-env.mjs";
 import { webPush } from "#src/lib/web-push";
-
-const pushSubscription = {
-  endpoint:
-    "https://fcm.googleapis.com/fcm/send/dpGu0URg-6o:APA91bHo1-gqU6g5y5_LRyGpp_f0w0EQAtIFkZv0kQsMkBXAVOsw8aiJ5hNjJRay7y2vbYwNHvfaEaPzypSRdIkDwm7f69D7shHFgqhCr6zfboUUQru-jxeRRmcHOg8ZSE_p5hw5vIsQ",
-  p256dh_base64url: "BLljo8JDDVZkDgYDNHbfm-dDRvjN4enJyz6wVMZ3bNAfbnZpLaVftQTes2GQXpd7S9vxOy66hkbbU2jsjMVoPzQ",
-  auth_base64url: "9P5ng2SyEmFlS3W1BfJOfQ",
-};
+import { type Message } from "#src/lib/cloud-messaging-light/notify";
+import { dbfetch } from "#src/db";
 
 async function main() {
+  const userId = BigInt(1);
+  const pushSubscription = await dbfetch()
+    .selectFrom("PushSubscription")
+    .where("userId", "=", userId)
+    .select(["auth_base64url", "p256dh_base64url", "endpoint"])
+    .executeTakeFirstOrThrow();
+
+  const message: Message = {
+    title: "some title 3",
+    body: "some body 3",
+    relativeLink: "/event/R3AxD",
+  };
+
   const res = await webPush({
-    payload: "hello",
+    payload: JSON.stringify(message),
     pushSubscription,
   });
   console.log(res);
